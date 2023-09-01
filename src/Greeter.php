f74012d1918b799b001f4aded2172f5d032ffbb7
RenKai <renkai@secretlisa.com>
2023-09-01 18:37:32 +0800

    fix greeter file

--- src/Greeter.php
+++ src/Greeter.php
@@ -10,22 +10,6 @@ class Greeter
 			return "Hello, $name!";
 		}
 
-
-
-
-		if ($name == 'asss') exit;
-
-		if ($name == 'aaaaa') {
-			
-			sleep(1);
-
-			if (strlen($name) == 5) 
-			return 'hi';
-			
-			
-			
-	sleep(2);
-		}
 		return "Hello!" ;  
 	}
 }
