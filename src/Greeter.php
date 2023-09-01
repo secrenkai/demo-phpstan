223bd54b4617eef213d38f6df4601ac9b0cdbae1
RenKai <renkai@secretlisa.com>
2023-09-01 17:40:38 +0800

    test

--- src/Greeter.php
+++ src/Greeter.php
@@ -1,12 +1,23 @@
 <?php
+
 namespace App;
 
-class Greeter {
-	public function greet(string $name = null):string {
-		if($name) {
+class Greeter
+{
+	public function greet(string $name = null): string
+	{
+		if ($name) {
 			return "Hello, $name!";
 		}
 
-		return "Hello!";
+
+
+
+		if ($name == 'asss') exit;
+
+		if ($name == 'aaaaa') {
+			if (strlen($name) == 5) return 'hi';
+		}
+		return "Hello!" ;  
 	}
 }
