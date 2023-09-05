0816dbadcabab787df9e7a6f3daf25b85d9ba352
RenKai <renkai@secretlisa.com>
2023-09-05 16:08:04 +0800

    php test2

--- inc/Example.php
+++ inc/Example.php
@@ -3,14 +3,16 @@ class Example
 {
     public function getReason(): string
     {
+        $this->test();
         return <<<STR
 The reason for this class to exist is to act as documentation
 for passing multiple directories to PHPStan.
 Have a great day!
 STR;
+        
     }
     private function test()
     {
-        echo "s"ssvr
+        echo "s";
     }
 }
